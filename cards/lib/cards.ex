b3f9c2925074416944441f07bd48446906d2483d
Sermed Al-abbasi <sermed@omicron.se>
2018-03-05 15:31:26 +0100

    Add existing file

---
+++ cards/lib/cards.ex
@@ -0,0 +1,58 @@
+defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """ 
+    Returns a list of stringsrepresenting a deck of playing cards
+  """
+  def create_deck do
+    values = ["Ace","Two","Three","Four","Five"]
+    suits = ["Spades","Clubs","Hearts","Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+  @doc """
+    Diveds a deck into a hand and remainder of hte deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+  
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+  def save(deck, filename) do
+     binary = :erlang.term_to_binary(deck)
+     File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "The file doesn't exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck 
+    |> Cards.shuffle 
+    |> Cards.deal(hand_size)
+  end
+end
